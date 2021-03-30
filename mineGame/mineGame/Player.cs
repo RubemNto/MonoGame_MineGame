ce945c1e52236fbc4b643a33674ef47aa2f63094
Alexpicarvalho <59317333+Alexpicarvalho@users.noreply.github.com>
2021-03-30 16:13:35 +0100

    Update Player.cs

--- mineGame/mineGame/Player.cs
+++ mineGame/mineGame/Player.cs
@@ -12,6 +12,7 @@ namespace mineGame
         //private Point _position;
         private Game1 game;
         private bool _keysReleased = true;
+        private bool dead = false;
         bool moved = false;
         private char[] _direction = {
             'L','U','D', 'R'
@@ -21,8 +22,11 @@ namespace mineGame
 
         public Texture2D texture;
         public Vector2 _position;
-        private Vector2 _movementDestination;
+        private Vector2 initialPos;
+        public Vector2 _movementDestination;
         public float speed;
+        public float deadTimer = 2;
+
         public bool faceRight = true;
         SoundEffect moveSound;
         SoundEffect sandDestructionSound;
@@ -38,14 +42,22 @@ namespace mineGame
 
             texture = g.Content.Load<Texture2D>("player");
             _position = position;
+            initialPos = position;
             _movementDestination = _position;
         }
 
 
-        public void update(GameTime gameTime,ref bool pressingDown) 
+        public void update(Game1 game, GameTime gameTime,ref bool pressingDown) 
         {
-            checkOrientation(ref pressingDown);
-            moveTo(ref _position,_movementDestination, speed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            if(dead == false) { 
+                checkOrientation(ref pressingDown);
+                moveTo(ref _position,_movementDestination, speed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+            else
+            {
+                resetPos(game, gameTime, ref deadTimer); 
+                
+            }
         }
 
         void moveTo(ref Vector2 originalPos,Vector2 destination,float Speed) 
@@ -116,18 +128,16 @@ namespace mineGame
                     faceRight = true;
                     _movementDestination.Y += game.tileSize;
                     pressingKeyDown = true;
-                }
-
-
-                if (pressingKeyDown)
-                    checkRocks();
+                } 
+                if(pressingKeyDown)
+                checkRocks();
 
                 ////check collision with walls
                 if (freeSpace() == false && pressingKeyDown == true)
                 {
                     _movementDestination = pastPosition;
                     moved = false;
-                }               
+                }
 
                 //play sound when move
                 if (_movementDestination != pastPosition && pressingKeyDown == true) 
@@ -177,10 +187,10 @@ namespace mineGame
             }
             for (int i = 0; i < game.GM.rocks.Count; i++)
             {
-                    if (game.GM.rocks[i].pos == _movementDestination)
-                    {
-                        return false;
-                    } 
+                if (game.GM.rocks[i].pos == _movementDestination)
+                {
+                    return false;
+                }
             }
             if (_movementDestination.X > game.windowHeight - 1 || _movementDestination.X < 0)
             {
@@ -195,23 +205,130 @@ namespace mineGame
             return true;
         }
 
+        public bool NextTo(Vector2 Destination)
+        {
+            Vector2[] positions = {
+                new Vector2(_movementDestination.X,_movementDestination.Y+game.tileSize),
+                new Vector2(_movementDestination.X,_movementDestination.Y-game.tileSize),
+                };
+            //Console.WriteLine(positions[0]);
+            //Console.WriteLine(positions[1]);
+            //Console.WriteLine(Destination);
+
+            //Thread.Sleep(2000);
+            //Console.Clear();
+
+            foreach (Rock rock in game.GM.rocks)
+            {
+                if (rock.pos == Destination)
+                {
+                    if ((Destination - new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[0]) || (Destination + new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[1]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            foreach (Sand sand in game.GM.sands)
+            {
+                if (sand.pos == Destination)
+                {
+                    if ((Destination - new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[0]) || (Destination + new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[1]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            foreach (wall Wall in game.GM.walls)
+            {
+                if (Wall.pos == Destination)
+                {
+                    if ((Destination - new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[0]) || (Destination + new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[1]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            foreach (Bomb bomb in game.GM.bombs)
+            {
+                if (bomb.pos == Destination)
+                {
+                    if ((Destination - new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[0]) || (Destination + new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[1]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            foreach (Diamond diamond in game.GM.diamonds)
+            {
+                if (diamond.pos == Destination)
+                {
+                    if ((Destination - new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[0]) || (Destination + new Vector2(Destination.X, Destination.Y + game.tileSize) == positions[1]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
         public void checkRocks()
-        {            
+        {
             //get all rocks in list
             for (int i = 0; i < game.GM.rocks.Count; i++)
             {
                 if (game.GM.rocks[i].pos == _position + new Vector2(0, game.tileSize) && _dir == 'R') //check rocks at the right
-                {                    
+                {
+                    
                     game.GM.rocks[i].updatePosition(game.GM.rocks[i].pos + new Vector2(0, game.tileSize),game);
                     //_movementDestination = new Vector2(_position.X, _position.Y + 32);
                     
                 }
                 else if (game.GM.rocks[i].pos == _position - new Vector2(0, game.tileSize) && _dir == 'L') //check rocks at the left
-                {                    
+                {
+                    
                     game.GM.rocks[i].updatePosition(game.GM.rocks[i].pos - new Vector2(0, game.tileSize),game);
                     //_movementDestination = new Vector2(_position.X, _position.Y - 32);
                 }
             }
+        }
+        public void deadPlayer(Game1 g)
+        {
+            Texture2D tempTexture = g.Content.Load<Texture2D>("playerDead");
+
+            // código para -1 vida, verificar se vidas são maiores ou = a zero
+
+            //Apresentar o "Game Over" caso vidas = 0
+
+            //Function to kill the player
+
+
+
+            dead = true;
+            texture = tempTexture;            
+           
+
+        }
+        
+        public void resetPos(Game g, GameTime gameTime, ref float _deadTimer)
+        {
+            float time = 2;
+
+            if (_deadTimer <= 0)
+            {
+                texture = g.Content.Load<Texture2D>("player");
+                _movementDestination = initialPos;
+                _position = initialPos;
+                _deadTimer = time;
+                dead = false;
+            }
+            else
+            {
+                _deadTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            
+
+
         }
 
     }
